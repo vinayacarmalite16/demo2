3c2b61ec60796e3e558d9f2f98ae7adb4acadf10
vinayacarmalite16 <vinayacarmalite@gmail.com>
2024-04-09 10:03:21 +0530

    second commit

---
+++ src/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    private String name;
+    public String getName(){
+        return name;
+    }
+    public void setName(String name) {
+        if (name != null && !name.isEmpty()) {
+            this.name = name;
+
+        } else {
+            System.out.println("Invalid");
+        }
+    }
+
+}
